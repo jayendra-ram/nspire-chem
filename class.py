1353b08cbbe097480c9f8d469af30bca7e86f290
Soycid <42985072+Soycid@users.noreply.github.com>
2018-09-05 17:23:21 -0500

    basic class intiailization

---
+++ class.py
@@ -0,0 +1,20 @@
+class element:
+    def __init__(self,an,mm,):
+        self.mm = mm
+        self.an = an
+H = element(1,1.01)
+He = element(2,4.00)
+Li = element(3,6.94)
+Be = element(4,9.01)
+B = element(5,10.81)
+C = element(6,12.01)
+N = element(7,14.01)
+O = element(8,16.00)
+F = element(9,19.00)
+Ne = element(10,20.18)
+Na = element(11,22.99)
+Mg = element(12,24.31)
+Al = element(13,26.98)
+Si = element(14,28.09)
+P = element(15,30.97)
+S = element(16,32.07)

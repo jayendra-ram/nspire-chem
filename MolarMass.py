b4dc880bb642b1c06b239b4a7ad65ac7ae4d4ceb
Tetzel <treyetzel@gmail.com>
2018-09-06 19:59:34 -0500

    Recreated Function, FUNCTIONAL BUT NEEDS TESTING

--- MolarMass.py
+++ MolarMass.py
@@ -1,22 +1,30 @@
 from ElementInfoClass import *
 def MolarMass(formula):
-  l = len(formula)
+  formula += "   "
   temp = ""
-  count = 0
   mass = 0
+  tempMass = 0
+  amount = 1
   for i,c in enumerate(formula):
     if(c.istitle()):
       temp += c
-      count = 1
-    if(not c.istitle()): 
-      temp += c
-      count +=1
-    if(c.isdigit())
-      if(not formula[i+1].isdigit()):
-        
-      else:
-        mass += MolarMass(temp)
-  return mass
-  print(eval(formula + '.atomicMass'))
+      if(formula[i+1].isdigit()):
+        amount = int(formula[i+1])
+        if(formula[i+2].isdigit()):
+          amount = (10 * int(formula[i+1])) + int(formula[i+2])
+      if(formula[i+1].islower()):
+        temp += formula[i+1]
+        if(formula[i+2].isdigit()):
+          amount = int(formula[i+1])
+          if(formula[i+3].isdigit()):
+            amount = (10 * int(formula[i+2])) + int(formula[i+3])
+      tempMass = eval(temp + '.atomicMass')
+      tempMass *= amount
+      mass += tempMass
+      temp = ""
+      tempMass = 0
+      amount = 1
+  return round(mass,2)
 
-string = "CaCl2"
+#NOTE:
+#-Subscript following variable cannot be larger than 100

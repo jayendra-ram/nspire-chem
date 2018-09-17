bd5612c535fcf96078c810003813758c6614b373
Tetzel <treyetzel@gmail.com>
2018-09-17 08:04:30 -0500

    Updated to be Function

--- LimitingReactant.py
+++ LimitingReactant.py
@@ -1,3 +1,25 @@
 from ElementInfoClass import *
 from MolarMass import*
-def LimitingReactant():
+def LimitingReactant(element1,amount1,coef1,*args):
+  mm = MolarMass(element1)
+  tempval = amount1 / mm
+  tempval /= coef1
+  val = tempval
+  tempel = element1
+  el = tempel
+  mass = 0
+  for arg in args:
+    if(type(arg) is str):
+      mm = MolarMass(arg)
+      tempel = arg
+    if(((type(arg) == int) or (type(arg) == float)) and mass == 1):
+      tempval = arg / mm
+    if(((type(arg) == int) or (type(arg) == float)) and mass == 2):
+      tempval /= arg
+      if(tempval <= val):
+        val = tempval
+        el = tempel
+    mass += 1
+    if(mass == 3):
+      mass = 0
+  return(el)
